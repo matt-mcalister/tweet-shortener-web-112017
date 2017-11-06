1c148330a3c875bf8fc621c64902ab23db610b90
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-06 20:11:20 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  dictionary
+  tweet.split.collect do |word|
+    if dictionary.keys.include?(word)
+      word = dictionary[word]
+    end
+  end.join
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
