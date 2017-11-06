40d684978e5b0b905ada31fa59a73f6a040c5587
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-06 20:20:09 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,12 +29,11 @@ def bulk_tweet_shortener(array)
 end
 
 
-def selective_tweet_shortener(array)
-  array.each do |tweet|
+def selective_tweet_shortener(tweet)
     if tweet.length > 140
-      puts word_substituter(tweet)
+      word_substituter(tweet)
     else
-      puts tweet
+      tweet
     end
   end
 end
