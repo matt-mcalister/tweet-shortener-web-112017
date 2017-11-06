a6d73754f123ce2f26646b0483571178bce724ec
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-06 20:30:54 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,7 +14,7 @@ end
 def word_substituter(tweet)
   dictionary
   result = ""
-  tweet.split.map do |word|
+  tweet.split.map! do |word|
     if dictionary.keys.include?(word)
       word = dictionary[word]
     else
